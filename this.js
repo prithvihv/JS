f0d8a07ac223c9c0c7b8fe3f9cccd7df46b4217f
Prithvi hv <hvprithvi09@gmail.com>
2017-10-19 22:44:31 +0530

    this key work

---
+++ this.js
@@ -0,0 +1,24 @@
+function a(){
+    this.newvariable="this was created inside a function";
+}
+a();
+console.log(newvariable);
+
+
+var c={
+    name :"new object",
+    log :function(){
+        //var self = this;
+        //replace this with self everywhere to produce the required results
+        this.name= "object changed up log function";
+        console.log(this);
+        var setname = function(){
+            this.name ="function inside a function ";
+        }
+        setname();
+        console.log(this);
+    }   
+
+}
+console.log("name in global : " + name );
+c.log();
